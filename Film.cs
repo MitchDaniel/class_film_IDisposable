e88e0801e4018037a09703069e85ea85cad9e281
mitchdaniel <mars529440@gmail.com>
2024-07-24 14:21:31 +0300

    Final commit

---
+++ Film.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice_modul_11
+{
+    internal class Film : IDisposable
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Studio { get; set; } = string.Empty;
+
+        public string Ganre { get; set; } = string.Empty;
+
+        public TimeOnly Duration { get; set; }
+
+        public int Year { get; set; }
+
+        public Film(string name, string studio, string ganre, TimeOnly duration, int year)
+        {
+            Name = name;
+            Studio = studio;
+            Ganre = ganre;
+            Duration = duration;
+            Year = year;
+        }
+
+        ~Film()
+        {
+            Dispose(false);
+            Console.WriteLine("Destructor");
+            // какое задание, такой и деструктор
+        }
+
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Console.WriteLine($"Rip film {Name}");
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing)
+            {
+                Console.WriteLine("Освобождаю ресурсы. Отстыковка");
+            }
+            disposed = true;
+        }
+    }
+}
+
+//Создайте класс Фильм. Класс должен хранить такую информацию:
+//-Название фильма
+//-Название киностудии
+//-Жанр
+//-Длительность
+//-Год выпуска
+//Реализуйте в классе методы и свойства, необходимые для функционирования класса.
+//Добавьте в классе деструктор. Напишите код для тестирования функциональности класса.
+//Напишите код для вызова деструктора.
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp_Practice_modul_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(Film DeadpoolAndWolverine = new Film("Deadpool And Wolverine", "Marvel Studios", "Superhero", new TimeOnly(2, 6), 2024))
+            {
+                Console.WriteLine(DeadpoolAndWolverine.Name);
+            }
+           
+            Console.Read();
+        }
+    }
+}
+
+
+//Создайте класс Фильм. Класс должен хранить такую информацию:
+//-Название фильма
+//- Название киностудии
+//- Жанр
+//- Длительность
+//- Год выпуска
+//Реализуйте в классе методы и свойства, необходимые для функционирования класса.
+//Добавьте в классе деструктор. Напишите код для тестирования функциональности класса.
+//Напишите код для вызова деструктора.
